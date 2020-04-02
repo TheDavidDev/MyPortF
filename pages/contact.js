0f3c51d145a1419709e4ef4c755f7663a65bffc6
nayablone <nayab.lone@hyperisland.com>
2020-04-02 08:44:41 +0200

    next.js project

---
+++ pages/contact.js
@@ -0,0 +1,17 @@
+import Layout from '../components/Layout';
+
+const Contact = () => (
+  <Layout>
+    <div>
+      <h1>Contact</h1>
+      <p>Nayab Lone 
+         <br/> Email Nayab.Lone@hyperisland.se
+          <br/>Phone : 0738236700
+          <br/>Facebook: Ryangigs
+          
+      </p>
+    </div>
+  </Layout>
+);
+
+export default Contact;
---
+++ pages/index.js
@@ -0,0 +1,25 @@
+
+import Layout from '../components/Layout';
+
+const Index = () => 
+    <Layout>
+    <div>
+      
+                      <h1>Frontend Web Developer</h1>
+                    <h3>HTML/CSS | JavaScript | React | Github | Figma </h3>
+      <img 
+                    src="https://cdn2.iconfinder.com/data/icons/people-flat-design/64/Man-Person-People-Avatar-User-Happy-512.png"
+                    alt="avatar"
+                    className="avatar-img"
+                    />
+      
+    </div>
+                    
+                    
+
+   </Layout>
+
+   
+
+
+export default Index;
