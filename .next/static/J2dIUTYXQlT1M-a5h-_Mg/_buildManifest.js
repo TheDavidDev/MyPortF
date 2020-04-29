71c3dc64edfc0b15e6614f9948f3fb6647e52209
𝔻ℕ𝕠𝕣𝕕𝕚𝕔𝕍𝕫𝕝𝕒𝕟 <sthlm412@gmail.com>
2020-04-29 19:36:00 +0200

    npm rebuild

---
+++ .next/static/J2dIUTYXQlT1M-a5h-_Mg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/contact":[a],"/index":[a],"/photo":[a],"/projects":[a]}}("static\u002Fchunks\u002Fa26269aa360fccb717dd4bb2cc6eee86024ff380.fde6add86bd68df86d22.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
